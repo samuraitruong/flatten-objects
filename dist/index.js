9e366896c1043c50ba856abe504592e8b0c41954
Truong Nguyen <samuraitruong@hotmail.com>
2021-11-09 21:02:49 +1100

    feat: add gitignore

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.flattenObject = void 0;
-function flattenObject(input) {
-    return [input];
-}
-exports.flattenObject = flattenObject;
